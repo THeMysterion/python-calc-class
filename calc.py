149d2865799cc809f254b9d19cfb12c5021477be
THeMysterion <dorianpolska153@gmail.com>
2017-12-30 19:35:16 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+class Calculator():
+    
+    def add(self, a, b):
+        score = a + b
+        print(score)
+        
+    def subtract(self, a, b):
+        score = a - b
+        print(score)
+        
+calc = Calculator()
+
+calc.add(10,15)
