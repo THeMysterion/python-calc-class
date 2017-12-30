55a7dbda8f76ecb4c85ec21571dda689c17d8fd9
THeMysterion <dorianpolska153@gmail.com>
2017-12-30 20:36:34 +0100

    Up1*calc.py
    added*
    : divider, multiplier

--- calc.py
+++ calc.py
@@ -1,5 +1,9 @@
 class Calculator():
     
+    def __init__(self):
+        print("Score:")
+         
+    
     def add(self, a, b):
         score = a + b
         print(score)
@@ -8,5 +12,15 @@ class Calculator():
         score = a - b
         print(score)
         
+    def multiplier(self, a, b):
+        score = a * b
+        print(score)
+        
+    def divider(self, a, b):
+        score = a / b
+        print(score)
+        
 calc = Calculator()
+calc
+    
 
